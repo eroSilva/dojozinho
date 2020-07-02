bdc7294a97055e18af96cac4f30c9a4362518c0d
Rafael Antonio Lucio <rafaelantoniolucio@gmail.com>
2020-07-02 17:17:29 -0300

    tests: add more tests in calculator component

--- bdd/react-bdd/src/components/Calculator/Calculator.js
+++ bdd/react-bdd/src/components/Calculator/Calculator.js
@@ -1,17 +1,64 @@
-import React, { useState } from 'react'
-import { Button } from './Button'
+import React, { useState } from "react";
+import { Button } from "./Button";
 
 const Calculator = () => {
-  const [displayValue, setDisplayValue] = useState(0);
+  const [displayValue, setDisplayValue] = useState("0");
 
-  const behaviorClick = value => setDisplayValue(value) 
+  const behaviorClick = (value) => {
+    if (value === 'clear') {
+      setDisplayValue(0);
+      return;
+    }
+
+    if (value === '=') {
+      setDisplayValue(eval(displayValue));
+      return;
+    }
+
+    if (displayValue === "0") {
+      setDisplayValue(value);
+      return;
+    }
+
+    setDisplayValue(displayValue.toString() + value.toString());
+  };
 
   return (
     <>
-      <input type="text" data-testid="display" value={displayValue}/>
-      <Button data-testid="button1" onClick={() => behaviorClick(1)}>1</Button>
+      <input type="text" data-testid="display" value={displayValue} />
+      <Button data-testid="button0" onClick={() => behaviorClick(0)}>
+        0
+      </Button>
+      <Button data-testid="button1" onClick={() => behaviorClick(1)}>
+        1
+      </Button>
+      <Button data-testid="button2" onClick={() => behaviorClick(2)}>
+        2
+      </Button>
+      <Button data-testid="button3" onClick={() => behaviorClick(3)}>
+        3
+      </Button>
+      <Button data-testid="buttonPlus" onClick={() => behaviorClick('+')}>
+        +
+      </Button>
+      <Button data-testid="buttonSubtraction" onClick={() => behaviorClick('-')}>
+        -
+      </Button>
+      <Button data-testid="buttonDivision" onClick={() => behaviorClick('/')}>
+        /
+      </Button>
+      <Button data-testid="buttonEqual" onClick={() => behaviorClick('=')}>
+        =
+      </Button>
+      <Button data-testid="buttonClear" onClick={() => behaviorClick('clear')}>
+        Limpar
+      </Button>
     </>
-  )
-}
+  );
+};
 
-export { Calculator }
+export { Calculator };
+
+
+
+
--- bdd/react-bdd/src/components/Calculator/__tests__/Calculator.test.js
+++ bdd/react-bdd/src/components/Calculator/__tests__/Calculator.test.js
@@ -5,24 +5,186 @@ import { render, fireEvent, screen } from "@testing-library/react";
 import { Calculator } from "../Calculator";
 
 describe("Calculator", () => {
-  test("should render calculator on display equal 0", () => {
-    // arrange
-    render(<Calculator />);
+  describe("Display", () => {
+    it("when render calculator should show 0", () => {
+      // arrange
+      render(<Calculator />);
 
+      // assert
+      expect(screen.getByTestId('display').value).toEqual("0")
+    });
 
-    expect(screen.getByTestId('display').value).toEqual("0")
-  });
+    it("when click button 1 should show 1", () => {
+      // arrange
+      render(<Calculator />);
+      const button = screen.getByTestId('button1');
 
-  test("should click button one and modify input value to 1", () => {
-    // arrange
-    render(<Calculator />);
-    const button = screen.getByTestId('button1');
+      // act
+      fireEvent.click(button)
 
-    // act
-    fireEvent.click(button)
+      // assert
+      expect(screen.getByTestId('display').value).toEqual("1")
+    });
 
-    // assert
-    expect(screen.getByTestId('display').value).toEqual("1")
-  });
+    it("when click button + should show +", () => {
+      // arrange
+      render(<Calculator />);
+      const button = screen.getByTestId('buttonPlus');
 
+      // act
+      fireEvent.click(button)
+
+      // assert
+      expect(screen.getByTestId('display').value).toEqual("+")
+    });
+
+    it("when click button 1, + and 1 again, should show 1+1", () => {
+      // arrange
+      render(<Calculator />);
+      const buttonPlus = screen.getByTestId('buttonPlus');
+      const buttonOne = screen.getByTestId('button1');
+
+      // act
+      fireEvent.click(buttonOne)
+      fireEvent.click(buttonPlus)
+      fireEvent.click(buttonOne)
+
+      // assert
+      const buttonTwo = screen.getByTestId('button2');
+      expect(screen.getByTestId('display').value).toEqual("1+1")
+    });
+  })
+
+  describe("Clear", () => {
+    it("when click button clear should change display to 0", () => {
+      // arrange
+      render(<Calculator />);
+      const buttonOne = screen.getByTestId('button1');
+      const buttonTwo = screen.getByTestId('button2');
+      const buttonClear = screen.getByTestId('buttonClear');
+
+      // act
+      fireEvent.click(buttonOne)
+      fireEvent.click(buttonTwo)
+      fireEvent.click(buttonClear)
+
+      // assert
+      expect(screen.getByTestId('display').value).toEqual("0")
+    });
+  })
+
+  describe("Addition", () => {
+    it("when click button 1, +, 1 and = should display 2", () => {
+      // arrange
+      render(<Calculator />);
+      const buttonPlus = screen.getByTestId('buttonPlus');
+      const buttonTwo = screen.getByTestId('button2');
+      const buttonOne = screen.getByTestId('button1');
+      const buttonEqual = screen.getByTestId('buttonEqual');
+
+      // act
+      fireEvent.click(buttonOne)
+      fireEvent.click(buttonPlus)
+      fireEvent.click(buttonOne)
+      fireEvent.click(buttonEqual)
+
+      // assert
+      expect(screen.getByTestId('display').value).toEqual("2")
+    });
+
+    it("when click button 1, +, 2, =, +, 3 and = should display 6", () => {
+      // arrange
+      render(<Calculator />);
+      const buttonOne = screen.getByTestId('button1');
+      const buttonTwo = screen.getByTestId('button2');
+      const buttonThree = screen.getByTestId('button3');
+      const buttonPlus = screen.getByTestId('buttonPlus');
+      const buttonEqual = screen.getByTestId('buttonEqual');
+      // act
+      fireEvent.click(buttonOne)
+      fireEvent.click(buttonPlus)
+      fireEvent.click(buttonTwo)
+      fireEvent.click(buttonEqual)
+      fireEvent.click(buttonPlus)
+      fireEvent.click(buttonThree)
+      fireEvent.click(buttonEqual)
+  
+      expect(screen.getByTestId('display').value).toEqual("6")
+    });
+  })
+
+  describe("Subtraction", () => {
+    it("when click button 1 , - , 1 should = 0", () => {
+      // arrange
+      render(<Calculator />);
+      const buttonOne = screen.getByTestId('button1');
+      const buttonSubtraction = screen.getByTestId('buttonSubtraction');
+      const buttonEqual = screen.getByTestId('buttonEqual');
+
+  
+      // act
+      fireEvent.click(buttonOne)
+      fireEvent.click(buttonSubtraction)
+      fireEvent.click(buttonOne)
+      fireEvent.click(buttonEqual)
+  
+      // assert
+      expect(screen.getByTestId('display').value).toEqual("0")
+    });
+  })
+
+  describe("Division", () => {
+    it('when click button 2 / 2 should return 1', () => {
+      // arrange
+      render(<Calculator />);
+      const buttonTwo = screen.getByTestId('button2');
+      const buttonDivision = screen.getByTestId('buttonDivision');
+      const buttonEqual = screen.getByTestId('buttonEqual');
+
+      // act
+      fireEvent.click(buttonTwo)
+      fireEvent.click(buttonDivision)
+      fireEvent.click(buttonTwo)
+      fireEvent.click(buttonEqual)
+
+      // assert
+      expect(screen.getByTestId('display').value).toEqual("1")
+    });
+    
+    it('when click button 0 / 2 should return 0', () => {
+      // arrange
+      render(<Calculator />);
+      const buttonZero = screen.getByTestId('button0');
+      const buttonTwo = screen.getByTestId('button2');
+      const buttonDivision = screen.getByTestId('buttonDivision');
+      const buttonEqual = screen.getByTestId('buttonEqual');
+
+      // act
+      fireEvent.click(buttonZero)
+      fireEvent.click(buttonDivision)
+      fireEvent.click(buttonTwo)
+      fireEvent.click(buttonEqual)
+
+      // assert
+      expect(screen.getByTestId('display').value).toEqual("0")
+    });
+
+    it('when click button 2 / 0 should return Infinity', () => {
+      // arrange
+      render(<Calculator />);
+      const buttonZero = screen.getByTestId('button0');
+      const buttonTwo = screen.getByTestId('button2');
+      const buttonDivision = screen.getByTestId('buttonDivision');
+      const buttonEqual = screen.getByTestId('buttonEqual');
+
+      // act
+      fireEvent.click(buttonTwo)
+      fireEvent.click(buttonDivision)
+      fireEvent.click(buttonZero)
+      fireEvent.click(buttonEqual)
+
+      // assert
+      expect(screen.getByTestId('display').value).toEqual("Infinity")
+    });
+  })
 });
